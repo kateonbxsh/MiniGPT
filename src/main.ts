d4eb23d8ff132f016c5f07f63895f585b902749e
Aouab <aouabsss@gmail.com>
2025-01-07 22:10:03 +0100

    add training data and main function to test

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,71 @@
-import NeuralNetwork from "./lib/NeuralNetwork";
-import {TrainingMethod} from "./lib/builder/NeuralNetworkTrainingSession";
-import NeuralLayer from "./lib/NeuralLayer";
+import MultiLayerPerceptron from "@/lib/models/mlp/MultiLayerPerceptron";
+import GPT from "./lib/models/gpt/GPT";
+import AttentionBlock from "./lib/models/components/AttentionBlock";
+import ReLU from "./lib/activation/ReLU";
+import salutations from "./data/salutations";
+import Softmax from "./lib/activation/Softmax";
+import LeakyReLU from "./lib/activation/LeakyReLU";
+import NeuralLayer from "./lib/models/components/NeuralLayer";
+import MinMaxNorm from "./lib/activation/MinMaxNorm";
+import LayerNorm from "./lib/models/components/LayerNorm";
+import Sigmoid from "./lib/activation/Sigmoid";
+import GELU from "./lib/activation/GELU";
+import FeedForwardBlock from "./lib/models/mlp/FeedForwardBlock";
 
 function main() {
 
-    const myNetwork = NeuralNetwork.Builder
-        .setInput(5)
-        .setOutput(1)
-        .build();
+    const tokenSize = 16;
+    const gpt = new GPT(50, tokenSize, 0.001);
 
-    let session = myNetwork.startTrainingSession()
-        .setMethod(TrainingMethod.STOCHASTIC)
-        .setLearningRate(0.001);
+    gpt.addBlock(new AttentionBlock(tokenSize));
+    gpt.addBlock(new LayerNorm(tokenSize));
+    gpt.addBlock(FeedForwardBlock.Builder
+        .setInput(tokenSize)
+        .addLayer(new NeuralLayer(2 * tokenSize), GELU)
+        .addLayer(new NeuralLayer(4 * tokenSize), Softmax)
+        .setOutput(tokenSize)
+        .build()
+    );
+    gpt.addBlock(new LayerNorm(tokenSize));
+    gpt.addBlock(new AttentionBlock(tokenSize));
+    gpt.addBlock(new LayerNorm(tokenSize));
+    gpt.addBlock(FeedForwardBlock.Builder
+        .setInput(tokenSize)
+        .addLayer(new NeuralLayer(2 * tokenSize), GELU)
+        .addLayer(new NeuralLayer(4 * tokenSize), Softmax)
+        .setOutput(tokenSize)
+        .build()
+    );
+    gpt.addBlock(new LayerNorm(tokenSize));
+    gpt.addBlock(new AttentionBlock(tokenSize));
+    gpt.addBlock(new LayerNorm(tokenSize));
 
-    for(let i = 0; i < 100000; i++) {
-        let input = [], sum = 0;
-        for(let j = 0; j < 5; j++) {
-            let n = Math.floor(Math.random() * 10 - 5);
-            sum += n;
-            input.push(n);
-        }
-        session.addData(input, [sum]);
+    let lastAverage = 0, positiveVariations = 0;
+    for(var i = 0; i >= 0; i++) {
+        let averageCost = 0, totalNb = salutations.length;
+        salutations.forEach(salutation => {
+            let cost = gpt.learnFromText(salutation);
+            if (isNaN(cost)) {
+                console.log("DIVERGED!");
+                process.exit(1);
+            } else {
+                averageCost+=cost/totalNb;
+            }
+        })
+        console.log("Average cost", averageCost, 
+            ", Epoch:", i+1, 
+            ", Cost variation:", (averageCost - lastAverage))
+        if ((averageCost - lastAverage) > 0) positiveVariations++;
+        else positiveVariations = 0;
+        lastAverage = averageCost;
+        if (positiveVariations > 20) break;
     }
-    session.end();
-
-    myNetwork.activate([15, 15, -15, 2, -3]);
-    console.log(myNetwork.toString());
-    myNetwork.activate([1, 11, -10, 1, -3]);
-    console.log(myNetwork.toString());
-    myNetwork.activate([15, 0, 0, 0, -3]);
-    console.log(myNetwork.toString());
-    myNetwork.activate([1, 5, 2, 2, -3]);
-    console.log(myNetwork.toString());
-    myNetwork.activate([0, 0, 0, 2, -3]);
-    console.log(myNetwork.toString());
-
+    
+    let startText = "Hello";
+    for(var i = 0; i < 20; i++) {
+        console.log(startText);
+        startText = gpt.predictNext(startText);
+    }
 
 }
 
